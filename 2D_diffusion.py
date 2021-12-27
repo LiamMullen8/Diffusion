01d1b342c843feb3a83a28e66b964077b14f3bff
LLiiaamm <66106514+LiamMullen8@users.noreply.github.com>
2021-12-27 17:32:11 -0500

    3d scatter plot,  very inefficient wrt time slice. need to memoize

--- 2D_diffusion.py
+++ 2D_diffusion.py
@@ -1,14 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
 g_size=int(input("Enter Grid Size:"))
 T_slice=int(input("Enter Time Slice:"))
 
 # this is so we can reference negatives
-Grid = [[[0.0 for j in range(-g_size, g_size+1)] for i in range(-g_size, g_size+1)] for N in range(0, T_slice+1)]
-print(Grid)
-print("+++++++++++++++++++++++++++++++\n")
+Grid = [[[0 for j in range(-g_size, g_size+1)] for i in range(-g_size, g_size+1)] for N in range(0, T_slice+1)]
 
 # Discrete Form
 def W(m,p,N):
+	
 	# probability of moving in each direction
 	pl=pr=pu=pd=0.25
 
@@ -18,7 +19,7 @@ def W(m,p,N):
 			return 1.0
 		else:
 			return 0.0
-	
+
 	w = pr*W(m-1, p, N-1) + pr*W(m+1, p, N-1) + pu*W(m, p-1, N-1) + pd*W(m, p+1, N-1)
 
 	return w
@@ -32,6 +33,26 @@ for T in range(0, T_slice+1):
 print(Grid)
 
 
+##########################
+# setup 3d plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x=[]
+
+# get each x,y,z tuple to plot
+# z is probability of being at coord (x,y)
+for i in range(-g_size, g_size+1):
+	for j in range(-g_size, g_size+1):
+		l= [i,j,Grid[T_slice][g_size - i][g_size - j]]
+		x.append(l)
+
+
+x=np.array(x)
+print(x)
+
+ax.scatter3D(x[:,0],x[:,1], x[:,2])
+fig.show()
 
 
 
@@ -55,17 +76,19 @@ print(Grid)
 
 
 
-# Continuous Form
-def U(x,y,t):
 
-	pl=pr=pu=pd=0.25
-	dx=dy=dt=1
 
-	if t==0:
-		if x==0 and y==0:
-			return 1
-		else:
-			return 0
-	u = pr*U(x-dx, y, t-dt) + pr*U(x+dx, y,  t-dt) + pu*U(x, y-dy,  t-dt) + pd*U(x, y+dy,  t-dt)
+# # Continuous Form
+# def U(x,y,t):
 
-	return u
+# 	pl=pr=pu=pd=0.25
+# 	dx=dy=dt=1
+
+# 	if t==0:
+# 		if x==0 and y==0:
+# 			return 1
+# 		else:
+# 			return 0
+# 	u = pr*U(x-dx, y, t-dt) + pr*U(x+dx, y,  t-dt) + pu*U(x, y-dy,  t-dt) + pd*U(x, y+dy,  t-dt)
+
+# 	return u
