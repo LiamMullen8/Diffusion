67f7a42134b60f54368e4262a9ef1f3d74345345
LLiiaamm <66106514+LiamMullen8@users.noreply.github.com>
2021-12-30 14:25:35 -0500

    Update 1D.py

--- 1D.py
+++ 1D.py
@@ -1,26 +1,79 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-D1=int(input("Enter 1D Lim:"))
 # 1-D random walk
 ##############################################
 
-def oneD(N):
-	y = [0 for i in range(0,N)]
+def oneD():
+	global y
 	
-	for i in range(1,N):
+	for i in range(1, len(y)):
 		r = np.random.uniform(0,1)
 		if r < 0.5:
 			y[i]=y[i-1] - 1
 		elif r > 0.5:
 			y[i]=y[i-1] + 1
+		# else:
+		# 	y[i]=y[i-1]
+	# print(y)
+
+
+def W(m,N):
+
+	# probability of moving in each direction
+	pl=pr=0.5
+
+	# only location possible at time=0 is (0,0)
+	if N==0:
+		if m==0:
+			return 1.0
 		else:
-			y[i]=y[i-1]
+			return 0.0
+
+	w = pr*W(m-1, N-1) + pl*W(m+1, N-1)
+	return w
+
+############################################################
+# Discrete Form of Probabilities in 1D
+# #NEED TO MEMOIZE##
+if __name__ == "__main__":
+
+	D1=int(input("Enter 1D Lim:"))
+	T_slice=int(input("Enter Time Slice:"))
+	# if(T_slice >= D1):
+	# 	raise Exception("Time slice must be within your specified range")
 	
-	print(y)
+	y = [0 for _ in range(0, max(D1,T_slice+1))]
+	oneD()
 
-	fig1 = plt.figure()
-	plt.plot(y)
-	fig1.show()
+	fig, axs = plt.subplots(2)
 
-oneD(D1)
+	axs[0].set_title("1D Random Walk")
+	axs[0].plot(y)
+	axs[0].plot(T_slice, y[T_slice], 'rx')
+
+	axs[1].set_title(f"Probability of being @ y = {y[T_slice]} for t = {T_slice}")
+	axs[1].axvline(x=y[T_slice], color='r')
+
+	###############	###############	###############
+
+	Grid = [[-1 for _ in range(-D1, D1+1)] for _ in range(0, T_slice+1)]
+
+	for T in range(0, T_slice+1):
+		for i in range(-D1, D1+1):
+				Grid[T][D1 - i] = W(i,T)
+
+
+	x=[]
+	# y is probability of being at coord (x)
+	for i in range(-D1, D1+1):
+			l = [i, Grid[T_slice][D1 - i]]
+			x.append(l)
+
+	x = np.array(x)
+	print(x)
+
+	axs[1].plot(x[:,0], x[:,1])
+
+	fig.show()
+
